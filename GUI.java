d2fa01b9b41174e7a2202146ac0dd166ac1115eb
CheTranqui <CheTranqui@users.noreply.github.com>
2021-07-21 22:25:09 -0500

    GUI update
    Incorporated GUI class where all GUI elements are created and manipulated.
    
    Roster and Registrar support GUI by telling it what needs to be created.

---
+++ GUI.java
@@ -0,0 +1,311 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.LinkedList;
+
+public class GUI {
+    private JPanel panel = new JPanel();
+    private Dimension idealDimensions = new Dimension(750, 450);
+    GridBagConstraints c = new GridBagConstraints();
+    public static Color labelFontColor = new Color(250, 230, 230);
+    public static Color panelBackgroundColor = new Color(0, 30, 50);
+    public static Color labelHighlightColor = new Color(30, 70, 90);
+    public static Font verdanaPlain = new Font("Verdana", Font.PLAIN, 12);
+    public static Font verdanaBold = new Font("Verdana", Font.BOLD, 12);
+    public Registrar reg;
+    public Roster402_v2 roster;
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    public GUI() {
+        JFrame frame = createFrame();
+        panel = createPanel();
+        c.fill = GridBagConstraints.NORTH;
+        frame.add(panel, BorderLayout.NORTH);
+
+        new Registrar(this);
+        roster = new Roster402_v2(this);
+        reg.setRoster(roster);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private JFrame createFrame() {
+        JFrame frame = new JFrame();
+        frame.setSize(idealDimensions);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Student Roster");
+        return frame;
+    }
+
+    private JPanel createPanel() {
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setPreferredSize(idealDimensions);
+        panel.setLayout(new GridBagLayout());
+        panel.setBackground(panelBackgroundColor);
+        return panel;
+    }
+
+    public void createTitleLabel(String labelText, int gridX, int gridY, boolean bold){
+        JLabel label = new JLabel(labelText);
+        label.setForeground(labelFontColor);
+        label.setFont(verdanaPlain);
+        if (bold){
+            label.setFont(verdanaBold);
+        }
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.gridx = gridX;
+        c.gridy = gridY;
+        panel.add(label,c);
+    }
+
+    public void createDropDown(String[] choices, int gridX, int gridY){
+        JComboBox<String> dropDown = new JComboBox<String>(choices);
+        dropDown.setForeground(labelFontColor);
+        dropDown.setBackground(labelHighlightColor);
+        dropDown.setFont(verdanaBold);
+        DefaultListCellRenderer dlcr = new DefaultListCellRenderer();
+        dlcr.setHorizontalAlignment(DefaultListCellRenderer.CENTER);
+        dropDown.setRenderer(dlcr);
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 1;
+        c.gridheight = 2;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.insets = new Insets(5,0,0,0); // spacing between labels
+        // set starting position to Last Name (index 2 in the array)
+        // since starting sort is by last name, this makes default selection and data presentation match
+        dropDown.setSelectedIndex(2);
+        dropDown.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                reg.sortBy(reg.students, (String) dropDown.getSelectedItem());
+                reg.sortBy(roster.roster, (String) dropDown.getSelectedItem());
+        }});
+        panel.add(dropDown, c);
+    }
+
+    public void createStudentLabels(LinkedList<Student> list, int columnGridX, int startingGridY){
+        // all labels will be in GridX position 1.
+        int gridY = startingGridY;
+        for (Student s : list){
+            createStudentLabel(list, s, columnGridX, gridY);
+            gridY++;
+        }
+    }
+
+    private void createStudentLabel(LinkedList<Student> list, Student s, int gridX, int labelGridY){
+        String studentText = reg.getStudentText(s);
+        JLabel label = new JLabel(studentText);
+        label.setBackground(panelBackgroundColor);
+        label.setForeground(labelFontColor);
+        label.setFont(verdanaPlain);
+        label.setOpaque(true);
+        String labelName;
+        if (list == reg.students){
+            labelName = "students" + reg.getLabelName(labelGridY);  // allows us to access label and update it later
+        }
+        else{
+            labelName = "roster" + reg.getLabelName(labelGridY);
+        }
+
+        label.setName(labelName);
+        // when label is clicked, swap that student from one linkedlist to the other
+        // and update the two GUI lists / show end result as sorted how user has chosen
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int id = Integer.parseInt((label.getText().split(" "))[1]);
+                Student myStudent = reg.getStudent(id, list);
+                reg.swapStudent(list, myStudent);
+                reCreateStudentLabels();
+                String sortingAttribute = (String)((JComboBox) panel.getComponent(2)).getSelectedItem();
+                reg.sortBy(reg.students, sortingAttribute);
+                reg.sortBy(roster.roster, sortingAttribute);
+            }
+            // highlight label on mouseover
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                label.setBackground(labelHighlightColor);
+            }
+            // remove highlight on mouse exit
+            @Override
+            public void mouseExited(MouseEvent e) {
+                label.setBackground(panelBackgroundColor);
+            }
+        });
+
+        c.insets = new Insets(1,15,1,0); // spacing between labels
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.gridx = gridX;
+        c.gridy = labelGridY;  // used in label name, but also to set current position on grid
+        panel.add(label, c);
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    public void createPrintRosterButton() {
+        JButton printRoster = new JButton("Print Roster");
+        printRoster.setBackground(labelHighlightColor);
+        printRoster.setForeground(labelFontColor);
+        printRoster.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("\nCurrent Roster for CIS 402:\n");
+                if (roster.roster.size() == 0) {
+                    System.out.println("\tNo students are currently signed up for CIS 402.");
+                } else {
+                    int count = 1;
+                    for (Student s : roster.roster) {
+                        System.out.println("\t" + count + ".  " + reg.getStudentText(s));
+                        count++;
+                    }
+                }
+            }
+        });
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.gridx = 3;
+        c.gridy = 0;
+        panel.add(printRoster, c);
+        // buttons don't seem to be added to the panel without a repaint
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    private void removeLabels(){
+        Component[] p = panel.getComponents();  // array containing all panel components
+        for (int l = p.length-1; l > 0; l--){
+            p = panel.getComponents();  // array containing all panel components
+            for (int m = p.length-1; m > 0; m--) {
+                if (p[m].getName() != null &&
+                        (p[m].getName().equals("studentslabel" + l)
+                        || p[m].getName().equals("rosterlabel" + l))) {  // locate this label in particular
+                    panel.remove(m);
+                }
+            }
+        }
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    public void reCreateStudentLabels(){
+        removeLabels();
+        createStudentLabels(reg.students, 1,1);
+        createStudentLabels(roster.roster,2,1);
+    }
+
+    public void updateStudentLabels(LinkedList<Student> list){
+        // iterates through students in same manner as original creation
+        String listName;
+        if (list == reg.students){
+            listName = "students";
+        }
+        else{
+            listName = "roster";
+        }
+        int labelGridY = 0;  // represents the GridY position of the resulting label
+        for (Student s : list){
+            labelGridY++;
+            updateLabel(s, listName + reg.getLabelName(labelGridY));
+        }
+    }
+    // supports "updateStudentLabels"
+    private void updateLabel(Student s, String labelName){
+        Component[] p = panel.getComponents();  // array containing all panel components
+        for (int l = 0; l < p.length; l++){
+            if (p[l].getName() != null && p[l].getName().equals(labelName)){  // locate this label in particular
+                ((JLabel) p[l]).setText(reg.getStudentText(s));  // update its text
+            }
+        }
+    }
+
+    public void createRosterInstructionsLabel(String instructions){
+        JLabel rosterInstructionLabel = new JLabel(instructions);
+        rosterInstructionLabel.setForeground(new Color(220, 200, 200));
+        rosterInstructionLabel.setFont(verdanaPlain);
+        rosterInstructionLabel.setSize(new Dimension(60,150));
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 1;
+        c.gridheight = 5;
+        c.gridx = 0;
+        c.gridy = 3;
+        panel.add(rosterInstructionLabel, c);
+
+    }
+
+    public void createAddToRosterButton(){
+        JButton addToRoster = new JButton("Add to Roster");
+        addToRoster.setBackground(labelHighlightColor);
+        addToRoster.setForeground(labelFontColor);
+        addToRoster.addActionListener(new ActionListener() {
+            // when clicked, open input dialog to capture quantity to move
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean validInput = false;
+                String errorMessage = "";
+                do {
+                    String input = JOptionPane.showInputDialog(null, errorMessage + "How many students would you" +
+                            " like to add to the roster? (0 - " + reg.students.size() + ")");
+                    // allow for cancel/x/no input
+                    if (input == null){
+                        validInput = true;
+                    }
+                    // if input, make sure it's a proper amount and an actual integer
+                    else {
+                        try {
+                            int quantity = Integer.parseInt(input);
+                            if (quantity >= 0 && quantity <= reg.students.size()) {
+                                addToRoster(quantity);
+                                validInput = true;
+                            }
+                        } catch (Exception exception) {
+                            errorMessage = "Input must be a number between 0 and " + reg.students.size() + ".\n";
+                        }
+                    }
+                }while (!validInput);
+            }
+        });
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 1;
+        c.gridheight = 2;
+        c.gridx = 0;
+        c.gridy = 7;
+        panel.add(addToRoster, c);
+    }
+
+    private void addToRoster(int quantity){
+        for (int i = 0; i < quantity; i++) {
+            int index = (int) (Math.floor(Math.random() * reg.students.size()));
+            reg.swapStudent(reg.students, reg.students.get(index));
+        }
+        reCreateStudentLabels();
+        String sortingAttribute = (String)((JComboBox) panel.getComponent(2)).getSelectedItem();
+        reg.sortBy(reg.students, sortingAttribute);
+        reg.sortBy(roster.roster, sortingAttribute);
+    }
+    public void setRegistrar(Registrar r){
+        reg = r;
+    }
+    public void setRoster(Roster402_v2 r){
+        roster = r;
+    }
+}
--- Registrar.java
+++ Registrar.java
@@ -13,96 +13,28 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.*;
 
-public class Registrar implements ActionListener {
-
-    //class variables to be used in multiple places
-    public LinkedList<Student> students = new LinkedList<>();
-    public static Font verdanaPlain = new Font("Verdana", Font.PLAIN, 12);
-    public static Font verdanaBold = new Font("Verdana", Font.BOLD, 12);
-    public JPanel panel = new JPanel();
-    public static Color labelFontColor = new Color(250,230,230);
-    public static Color panelBackgroundColor = new Color(0,30,50);
-    public static Color labelHighlightColor = new Color(30, 70,90);
+public class Registrar{
     private Roster402_v2 myRoster;
+    private GUI gui;
+    public LinkedList<Student> students = new LinkedList<>();
 
-    public Registrar(){
+    public Registrar(GUI g){
+        setGUI(g);
+        gui.setRegistrar(this);
+
         LoadStudentsIntoList();
-        // creating a GUI for the roster display:
-        JFrame frame = new JFrame();
-        frame.setPreferredSize(new Dimension(750,400));
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-        panel.setLayout(new GridBagLayout());
-        panel.setBackground(panelBackgroundColor);
-        // using GridBagLayout to organize rows/columns
-        GridBagConstraints c = new GridBagConstraints();
-
     // intro label - gives a title to the dropdown - top left
-        JLabel sortByLabel = new JLabel("Sort By:");
-        sortByLabel.setForeground(labelFontColor);
-        sortByLabel.setFont(verdanaBold);
-        c.weightx = 0.5;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.gridx = 0;
-        c.gridy = 0;
-        panel.add(sortByLabel, c);
+        gui.createTitleLabel("Sort By:", 0,0, true);
 
         // intro label for students' column - top right
-        JLabel studentsLabel = new JLabel("Available Students:");
-        studentsLabel.setForeground(labelFontColor);
-        studentsLabel.setFont(verdanaBold);
-        c.weightx = 0.5;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.gridx = 1;
-        c.gridy = 0;
-        panel.add(studentsLabel, c);
+        gui.createTitleLabel("Available Students:", 1,0, true);
 
 // provide dropdown with options to sort by - under top left by 1
         String[] choicesForDropDown = {"Student ID", "First Name", "Last Name"};
-        JComboBox<String> sortByDropDown = new JComboBox<String>(choicesForDropDown);
-        sortByDropDown.setForeground(labelFontColor);
-        sortByDropDown.setBackground(labelHighlightColor);
-        sortByDropDown.setFont(verdanaBold);
-        c.weightx = 0.0;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.gridx = 0;
-        c.gridy = 1;
-        c.insets = new Insets(5,0,0,0); // spacing between labels
-        // set starting position to Last Name (index 2 in the array)
-        // since starting sort is by last name, this makes default selection and data presentation match
-        sortByDropDown.setSelectedIndex(2);
-        // when changed, will have to sort by choice
-        sortByDropDown.addActionListener(this);
-        panel.add(sortByDropDown, c);
+        gui.createDropDown(choicesForDropDown, 0,1);
 
-        createStudentLabels() ;
+        gui.createStudentLabels(students,1,1);
 
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Student Roster");
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public void createStudentLabels(){
-        // create labels to display current sorted roster
-        int labelGridY = 0;  // represents the GridY position of the resulting label
-        // all labels will be in GridX position 1.
-        GridBagConstraints c = new GridBagConstraints();
-        for (Student s : students){
-            labelGridY++;
-            createOutputLabel(s, labelGridY, c);
-        }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // cast source to JComboBox, sort via the text of the choice
-        JComboBox cb = (JComboBox)e.getSource();
-        sortBy(students, (String) cb.getSelectedItem());
-        sortBy(myRoster.roster, (String) cb.getSelectedItem());
     }
 
     public String getLabelName(int labelGridY){
@@ -113,55 +45,6 @@ public class Registrar implements ActionListener {
     public String getStudentText(Student s){
         // sets text of label to keep presentation uniform
         return "ID: " + s.getId() + " Name: " + s.getFirstName() + " " + s.getLastName();
-    }
-
-    private void createOutputLabel(Student s, int labelGridY, GridBagConstraints c){
-        String studentText = getStudentText(s);
-        JLabel label = new JLabel(studentText);
-        // dark mode!
-        label.setBackground(panelBackgroundColor);
-        label.setForeground(labelFontColor);
-        label.setOpaque(true);
-
-        String labelName = getLabelName(labelGridY);  // allows us to access label and update it later
-        label.setName(labelName);
-        label.setFont(verdanaPlain);
-
-        // when label is clicked, swap that student from one linkedlist to the other
-        // and update the two GUI lists / show end result as sorted how user has chosen
-        label.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                int id = Integer.parseInt((label.getText().split(" "))[1]);
-                Student myStudent = getStudent(id, students);
-                removeLabels("");
-                swapStudent(students, myStudent);
-                createStudentLabels();
-                myRoster.recreateRosterLabels();
-                String sortingAttribute = (String)((JComboBox) panel.getComponent(2)).getSelectedItem();
-                sortBy(students, sortingAttribute);
-                sortBy(myRoster.roster, sortingAttribute);
-            }
-            // highlight label on mouseover
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                label.setBackground(labelHighlightColor);
-            }
-            // remove highlight on mouse exit
-            @Override
-            public void mouseExited(MouseEvent e) {
-                label.setBackground(panelBackgroundColor);
-            }
-        });
-
-        c.insets = new Insets(5,15,5,0); // spacing between labels
-        c.weightx = 0.5;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.gridx = 1;
-        c.gridy = labelGridY;  // used in label name, but also to set current position on grid
-        panel.add(label, c);
     }
 
     // given an id and a list, return the relevant student
@@ -194,21 +77,6 @@ public class Registrar implements ActionListener {
         }
     }
 
-    // removes any (and only!) student labels from the GUI
-    public void removeLabels(String type){
-        Component[] p = panel.getComponents();  // array containing all panel components
-        for (int l = p.length-1; l > 0; l--){
-            p = panel.getComponents();  // array containing all panel components
-            for (int m = p.length-1; m > 0; m--) {
-                if (p[m].getName() != null && p[m].getName().equals(type + "label" + l)) {  // locate this label in particular
-                    panel.remove(m);
-                }
-            }
-        }
-        panel.revalidate();
-        panel.repaint();
-    }
-
     // sorts list provided by student attribute
     public void sortBy(LinkedList<Student> list, String studentAttribute){
         int totalIndexes = list.size()-1;
@@ -236,10 +104,10 @@ public class Registrar implements ActionListener {
             }
         }
         if (list == students) {
-            updateStudentLabels();
+            gui.updateStudentLabels(students);
         }
         else {
-            myRoster.updateRosterLabels();
+            gui.updateStudentLabels(myRoster.roster);
         }
     }
     // supporting method to assist with sorting
@@ -248,25 +116,6 @@ public class Registrar implements ActionListener {
         Student temp = list.get(studentIndex);
         list.set(studentIndex, list.get(studentIndex + 1));
         list.set(studentIndex + 1, temp);
-    }
-
-    // updates label info after sorting, rather than removing and recreating labels
-    public void updateStudentLabels(){
-        // iterates through students in same manner as original creation
-        int labelGridY = 0;  // represents the GridY position of the resulting label
-        for (Student s : students){
-            labelGridY++;
-            updateLabel(s, getLabelName(labelGridY));
-        }
-    }
-    // supports "updateStudentLabels"
-    private void updateLabel(Student s, String labelName){
-        Component[] p = panel.getComponents();  // array containing all panel components
-        for (int l = 0; l < p.length; l++){
-            if (p[l].getName() != null && p[l].getName().equals(labelName)){  // locate this label in particular
-                ((JLabel) p[l]).setText(getStudentText(s));  // update its text
-            }
-        }
     }
 
     // initial load of data into students linkedlist
@@ -286,5 +135,11 @@ public class Registrar implements ActionListener {
 
     public void setRoster(Roster402_v2 r){
         myRoster = r;
+    }
+    public void setGUI(GUI g){
+        gui = g;
+    }
+    public Registrar getRegistrar(){
+        return this;
     }
 }
